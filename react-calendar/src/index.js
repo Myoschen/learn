24cee4efea5221717539f9aa103b19fcd48a4442
willy14620 <willy14620@gmail.com>
2022-01-16 18:21:04 +0800

    2022-01-16

--- react-calendar/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import StyledApp from './App';
-
-ReactDOM.render(
-    <StyledApp className={'container'} />,
-    document.getElementById('root')
-);
